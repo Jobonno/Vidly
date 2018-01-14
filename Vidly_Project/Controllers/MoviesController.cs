6a18328b72964f4bdc56af8e37f5e91d38352e43
Joseph Bonifacio <jobonno@hotmail.com>
2018-01-15 00:05:16 +1100

    Added Movie properties, Genre Table and Seeded Genre Table

--- Vidly_Project/Controllers/MoviesController.cs
+++ Vidly_Project/Controllers/MoviesController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Configuration;
@@ -13,22 +14,26 @@ namespace Vidly_Project.Controllers
     public class MoviesController : Controller
     {
 
-        private List<Movie> movies = new List<Movie>
+        private ApplicationDbContext _context;
+
+        public MoviesController()
         {
-            new Movie() {Id= 1,Name = "Shrek"},
-            new Movie() {Id= 2,Name = "Wall-e"},
+            _context = new ApplicationDbContext();
+        }
 
-    };
-
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
         // GET: Movies/Random
         //public ActionResult Random()
         //{
         //    var movie = new Movie() {Name = "Shrek"};
-           
+
         //   // RandomMovieViewModel vm = new RandomMovieViewModel{Movie = movie, Customers = customers};
 
         //    return View(vm);
-            
+
         //}
 
         public ActionResult Edit(int id)
@@ -41,18 +46,18 @@ namespace Vidly_Project.Controllers
         {
             if (!pageIndex.HasValue) pageIndex = 1;
             if (String.IsNullOrWhiteSpace(sortBy)) sortBy = "Name";
-            MoviesIndexViewModel vm = new MoviesIndexViewModel {Movies = movies};
-            return View(vm);
+            var movies = _context.Movies.Include(g => g.Genre).ToList();
+            return View(movies);
         }
 
         [Route("movies/details/{id}")]
         public ActionResult Details(int id)
         {
-            Movie movie = movies.FirstOrDefault(c => c.Id == id);
+            var movie = _context.Movies.Include(g => g.Genre).FirstOrDefault(c => c.Id == id);
             if (movie != null)
             {
-                MovieDetailsViewModel vm = new MovieDetailsViewModel { Movie = movie };
-                return View(vm);
+                //MovieDetailsViewModel vm = new MovieDetailsViewModel { Movie = movie };
+                return View(movie);
             }
             else
             {
--- Vidly_Project/Models/Movie.cs
+++ Vidly_Project/Models/Movie.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Security.AccessControl;
 using System.Web;
 
 namespace Vidly_Project.Models
@@ -8,6 +10,17 @@ namespace Vidly_Project.Models
     public class Movie
     {
         public int Id { get; set; }
-        public string Name { get; set; }     
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public Genre Genre { get; set; }
+        [Required]
+        public byte GenreId { get; set; }
+        [Required]
+        public DateTime ReleaseDate { get; set; }
+        [Required]
+        public DateTime DateAdded { get; set; }
+        [Required]
+        public int NumberInStock { get; set; }
     }
 }
